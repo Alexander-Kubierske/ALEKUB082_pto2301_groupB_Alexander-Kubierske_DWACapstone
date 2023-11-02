754ea8ed40577ec1cbbc6e5d8d6e4c11f82f824c
Alexander Kubierske <alexanderkubierske@gmail.com>
2023-11-02 17:30:57 +0000

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+
+    return (
+        <nav>
+            <img src="" className="logo"></img> 
+            <img src="" className="search"></img>
+            <img src="" className="profile" />
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/components/index.tsx
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+
+interface Components {
+    Navbar: React.ComponentType;
+
+}
+
+const componentIndex: Components = {
+ Navbar,
+}
+
+export default componentIndex
+
+
+
+
